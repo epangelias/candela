c7362b47703e48b035f77606827bb4f5024e2eb8
Epangelias <epangelias@gmail.com>
2025-01-24 19:22:15 -0600

    fix

--- routes/(app)/api/search.ts
+++ routes/(app)/api/search.ts
@@ -2,16 +2,24 @@ import { define } from '@/lib/utils/utils.ts';
 
 import { OramaClient } from "npm:@oramacloud/client";
 
-const client = new OramaClient({
-    endpoint: "https://cloud.orama.run/v1/indexes/bible-dpqd9u",
-    api_key: "DR1FpYAOdKoXQcqlAUPO3kNyGRa47sRS",
-});
+let client: OramaClient;
+
+function getClient() {
+    if (!client) return client = new OramaClient({
+        endpoint: "https://cloud.orama.run/v1/indexes/bible-dpqd9u",
+        api_key: Deno.env.get("ORAMA_API_KEY")!,
+    });
+    return client;
+}
+
 
 
 
 export const handler = define.handlers({
     POST: async (ctx) => {
         const { query } = await ctx.req.json();
+
+        getClient();
 
         const resultsData = await client.search({
             term: query as string,
