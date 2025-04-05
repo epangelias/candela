eea89d1926a71d45e0b3d18483ecf0bf1757303f
Epangelias <epangelias@gmail.com>
2025-04-05 18:07:37 +0700

    fix prompt

--- routes/(app)/api/quick-explain.tsx
+++ routes/(app)/api/quick-explain.tsx
@@ -12,7 +12,7 @@ export const handler = define.handlers({
     if (!user) throw new HttpError(STATUS_CODE.Unauthorized);
 
     const prompt = query.split(' ').length > 1
-      ? `You will explain the meaning of the phrase in only 15 words, and very concisely. The selection will be in the language: ${user.language}, and your answer will be in the language ${user.nativeLanguage}. Do not repeat the selection, get straight to the point, and do not translate. Begin instantly from the first word the explanation or insight.`
+      ? `You will explain the meaning of the phrase in only 15 words, and very concisely. The selection will be in the language: ${user.language}, and your answer will be in the language ${user.nativeLanguage}. Do not repeat the selection, get straight to the point, and do not translate. Begin instantly from the first word the explanation or insight. Note that the origin of the selection comes from a biblical or other text.`
       : `You will provide a helpful definition for the word. The beginning of your response will be a list of 1-3 words separated by a comma that signify the meaning of the word. After that, a "." and relevant grammatical, etymological, or contextual insights in only 10 words. The selection will be in the language: ${user.language}, and your answer will be in the language ${user.nativeLanguage}`;
 
     return StreamAI({
